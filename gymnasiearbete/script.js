6434b42432d7be0d046e04508a3bd676fd80a14f
Hugge05 <75117937+Hugge05@users.noreply.github.com>
2024-01-16 13:15:44 +0100

    first commit

---
+++ gymnasiearbete/script.js
@@ -0,0 +1,31 @@
+
+document.querySelector(".meny").addEventListener("click", function()
+{
+    document.querySelector(".block").classList.toggle("active");
+    document.querySelector(".meny").classList.toggle("active");
+    document.querySelectorAll('.meny span')[0].classList.toggle("active");
+});
+/*
+document.querySelector(".starsidaLogo").addEventListener("scroll", function()
+{
+    document.querySelector(".startLogo").classList.toggle("active");
+    document.querySelector(".startsidaLogo").classList.toggle("active");
+    document.querySelector(" .startsidaLogo div")[0].classList.toggle("active");
+}); */
+// Header sida 2 
+document.querySelector(".meny").addEventListener("click", function()
+{
+    document.querySelector(".block").classList.toggle("active");
+    document.querySelector(".meny").classList.toggle("active");
+    document.querySelectorAll('.meny span')[0].classList.toggle("active");
+});
+// Header sida 3
+document.querySelector(".meny").addEventListener("click", function()
+{
+    document.querySelector(".block").classList.toggle("active");
+    document.querySelector(".meny").classList.toggle("active");
+    document.querySelectorAll('.meny span')[0].classList.toggle("active");
+});
+
+
+
